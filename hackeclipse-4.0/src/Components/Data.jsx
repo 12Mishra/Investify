7aaf3c9713289488775c07395bee15138dc8feab
Abhyuday Mishra <88658148+12Mishra@users.noreply.github.com>
2024-04-03 13:53:55 +0530

    button removal

--- hackeclipse-4.0/src/Components/Data.jsx
+++ hackeclipse-4.0/src/Components/Data.jsx
@@ -12,7 +12,7 @@ const Data = () => {
             <>
                 <h1><center>Invest in Top Companies</center></h1>
                 <h3><center>Online platform to invest in stocks, derivatives, mutual funds, and more</center></h3>
-                <button class="btn btn-outline-success me-2" type="button"></button><button className="center-button" onClick={redirect}><h6>Sign up now</h6></button>
+                <button className="center-button" onClick={redirect}><h6>Sign up now</h6></button>
 
             </>
         </div>
